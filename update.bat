da107a4e7f02aae6e0cac3c8beab0d8eea2ffa0a
DevThetHtwe <dev.thethtwe@gmail.com>
2023-06-24 16:37:48 +0630

    test

--- update.bat
+++ update.bat
@@ -3,6 +3,6 @@ date > C:\Users\NinjaCoder\Desktop\DevThetHtwe.github.io\canvaskit\dt.txt
 date > C:\Users\NinjaCoder\Desktop\DevThetHtwe.github.io\cc3\assets\common\css\dt.txt
 date > C:\Users\NinjaCoder\Desktop\DevThetHtwe.github.io\cc3\assets\vendors\bootstrap-sweetalert\dist\dt.txt
 git add .
-git commit -m "update"
+git commit -m "test"
 git push -u origin main
 pause > nul
